e1a19b8b322dc045614268148bfcb940763a785f
Vaibhav Bhasin <75664061+Vaibhav-Bhasin@users.noreply.github.com>
2021-12-19 13:40:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 function run(){
     var w = document.getElementById("weight");
-    var p = document.getElementById("planets");
-    console.log(w,p);
+    var p = document.getElementById("planetname");
+    var g = document.getElementById("result");
+    g.innerHTML=w.value+p.value;
+    console.log(w.value,p.value);
 }

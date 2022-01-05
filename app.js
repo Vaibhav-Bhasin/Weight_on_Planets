90d184da8360facdc6f947eb989836a0eeed3e59
Vaibhav Bhasin <75664061+Vaibhav-Bhasin@users.noreply.github.com>
2022-01-05 21:25:06 +0530

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-function run(){
-    var w = document.getElementById("weight");
-    var p = document.getElementById("planetname");
-    var g = document.getElementById("result");
-    g.innerHTML=w.value+p.value;
-    console.log(w.value,p.value);
-}
